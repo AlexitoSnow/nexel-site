9293b912905be4776ee4388a302d12e64b2fd92b
McLovin18 <hwcobena@espol.edu.ec>
2024-04-23 23:38:10 -0500

    mensahe xdxd

--- src/app/servicios/page.jsx
+++ src/app/servicios/page.jsx
@@ -1,8 +1,34 @@
+import Image from "next/image";
+import Button from "../components/Button";
+
 export default function Servicios() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl">Servicios</h1>
-            <p className="text-xl">Ofrecemos servicios de desarrollo web y móvil</p>
+        <main className="flex flex-col min-h-screen items-center justify-between p-24">
+            <h1 className="text-4xl mb-8 text-center md:text-left">
+                Tenemos una gran cantidad de servicios de los cuales podrás elegir
+            </h1>
+            <section className="flex flex-wrap justify-center space-x-4">
+                <div className="flex flex-col justify-center items-center w-full md:w-auto p-4 rounded-lg bg-black text-white">
+                    <Image className="w-1/2 h-1/2" src="/assets/images/sistema1.png" alt="" width={300} height={120} />
+                    <h1 className="text-lg">Tienda con registro</h1>
+                    <h1 className="text-lg">Inicio de sesión</h1>
+                    <Button text="Ver más"/>
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-full md:w-auto p-4 rounded-lg bg-black text-white">
+                    <Image className="w-1/2 h-1/2" src="/assets/images/servicio2.png" alt="" width={300} height={120} />
+                    <h1 className="text-lg">Tienda con</h1>
+                    <h1 className="text-lg">formulario</h1>
+                    <Button text="Ver más"/>
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-full md:w-auto p-4 rounded-lg bg-black text-white">
+                    <Image className="w-1/2 h-1/2" src="/assets/images/servicio3.png" alt="" width={300} height={120} />
+                    <h1 className="text-lg">Tienda con registro</h1>
+                    <h1 className="text-lg">Botón de pago</h1>
+                    <Button text="Ver más"/>
+                </div>
+            </section>
         </main>
     );
 }
--- src/app/sobre-nosotros/page.jsx
+++ src/app/sobre-nosotros/page.jsx
@@ -1,8 +1,39 @@
+import Image from "next/image";
+
 export default function SobreNosotros() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl">Sobre Nosotros</h1>
-            <p className="text-xl">Somos un grupo de freelancers</p>
+        <main className="flex min-h-screen flex-col">
+
+          <section className="px-5">
+
+            <section className="flex flex-col md:flex-row py-4 space-y-4">
+                <div className="w-1/2 space-y-4 flex flex-col justify-center">
+                    <h2 className="text-4xl font-bold text-center">¿Quienes somos?</h2>
+                    <p className="text-2xl">Somos un grupo de desarrolladores especializados en el desarrollo de tiendas online, con un fuerte compromiso en priorizar la experiencia de usuario.</p>
+                </div>
+                <Image src="" width={500} height={200} />
+            </section>
+
+            <section className="flex flex-col md:flex-row py-8">
+                <Image src="" width={500} height={200} />
+                <div className="w-1/2 space-y-4 flex flex-col justify-center">
+                    <h2 className="text-4xl font-bold text-center">Misión</h2>
+                    <p className="text-2xl">
+                    Nuestra misión es impulsar el crecimiento de emprendedores al proporcionar soluciones de desarrollo de tiendas online, permitiéndoles alcanzar su máximo potencial y ofreciendo a sus clientes una experiencia de compra excepcional
+                    </p>
+                </div>
+            </section>
+
+            <section className="flex flex-col md:flex-row py-4 space-y-4">
+              <div className="w-1/2 space-y-4 flex flex-col justify-center">
+                <h2 className="text-4xl font-bold text-center ">Visión</h2>
+                <p className="text-2xl">
+                    Nuestra visión es ser una empresa con alto impacto en la creación de tiendas online a nivel global
+                </p>
+              </div>
+              <Image src="" width={500} height={200} />
+            </section>
+          </section>
         </main>
-    );
+      );
 }
